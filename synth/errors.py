e0e4e14a9235e6d3d607ad3e845bdf34d894048a
Josh Humphries <j.humphries@nhm.ac.uk>
2020-09-21 23:04:09 +0100

    Choose a better word to make more sense

--- synth/errors.py
+++ synth/errors.py
@@ -2,7 +2,7 @@
 
 class SpecificDisciplineParentMismatch(Exception):
     """
-    This exception is caused when we're attempting to match up duplicate specific disciplines from
+    This exception is raised when we're attempting to match up duplicate specific disciplines from
     the 4 synth databases. We attempt to de-duplicate them by matching the name and this exception
     simply indicates that there was a name match between two synth databases but that the parent
     discipline of the specific disciplines that matched were different.
